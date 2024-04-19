fc29a8873dfc372749da849987aa7f8ec7284a29
d.cepil <db.cepil@gmail.com>
2024-04-19 20:31:41 +0200

    (feat)support for void vault

---
+++ Config.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace RodOfDiscordKeybind
+{
+    internal class Config : ModConfig
+    {
+        public override ConfigScope Mode => ConfigScope.ClientSide;
+
+        [Header("$Mods.RodOfDiscordKeybind.Config.Header")]
+
+        [DefaultValue(false)]
+        [ReloadRequired]
+        public bool VoidBagEnabled { get; set; }
+    }
+}
--- RodOfDiscordKeybind.cs
+++ RodOfDiscordKeybind.cs
@@ -24,30 +24,28 @@ namespace RodOfDiscordKeybind
 
     public class RodOfDiscordKeybindPlayer : ModPlayer
     {
-        private int _previousItem;
-        private bool _revertItem;
+        private int _rodItemIndex;
+        private int _previousItemIndex;
+        private bool _revertItemIndex;
+        private Item _swapItem;
+        private Item _rodItem;
+        private bool _isInVoidBag;
+        private readonly bool _voidBagEnabled = ModContent.GetInstance<Config>().VoidBagEnabled;
         private const int RodOfDiscordId = ItemID.RodofDiscord;
         private const int RodOfHarmonyId = ItemID.RodOfHarmony;
-
-        private void HandleRodUsage(int rodId)
-        {
-            _previousItem = Player.selectedItem;
-            _revertItem = true;
-            Player.selectedItem = Player.FindItem(rodId);
-            Player.controlUseItem = true;
-            Player.ItemCheck();
-        }
 
         public override void ProcessTriggers(TriggersSet triggersSet)
         {
             if (RodOfDiscordKeybind.Hotkey.JustPressed &&
                 Player.ItemTimeIsZero && Player.ItemAnimationEndingOrEnded)
             {
-                if (Player.HasItem(RodOfHarmonyId))
+                if (_voidBagEnabled ?
+                     Player.HasItemInInventoryOrOpenVoidBag(RodOfHarmonyId) : Player.HasItem(RodOfHarmonyId))
                 {
                     HandleRodUsage(RodOfHarmonyId);
                 }
-                else if (Player.HasItem(RodOfDiscordId))
+                else if (_voidBagEnabled ?
+                    Player.HasItemInInventoryOrOpenVoidBag(RodOfDiscordId) : Player.HasItem(RodOfDiscordId))
                 {
                     HandleRodUsage(RodOfDiscordId);
                 }
@@ -56,10 +54,59 @@ namespace RodOfDiscordKeybind
 
         public override void PostUpdate()
         {
-            if (_revertItem && Player.ItemTimeIsZero && Player.ItemAnimationEndingOrEnded)
+            if (_revertItemIndex && Player.ItemTimeIsZero && Player.ItemAnimationEndingOrEnded)
             {
-                Player.selectedItem = _previousItem;
-                _revertItem = false;
+                // Clean up after using the Rod Item - swap the items back around and cleanup vars
+                if (_isInVoidBag)
+                {
+                    Player.bank4.item[_rodItemIndex] = _rodItem;
+                    Player.inventory[_rodItemIndex] = _swapItem;
+                    _swapItem = null;
+                    _rodItem = null;
+                    _isInVoidBag = false;
+                }
+
+                Player.selectedItem = _previousItemIndex;
+                _revertItemIndex = false;
+            }
+        }
+
+        private void HandleRodUsage(int rodId)
+        {
+            _previousItemIndex = Player.selectedItem;
+            _revertItemIndex = true;
+            _rodItemIndex = _voidBagEnabled ?
+                Player.FindItemInInventoryOrOpenVoidBag(rodId, out _isInVoidBag) : Player.FindItem(rodId);
+            /*
+                FindItemInInventoryOrOpenVoidBag() will return -1 if it can't find the item.
+                For some reason it will falsely return -1 even when the item is there
+                when the player doesn't have an item at that index in their inventory,
+                so we have to check the Void Bag manually also,
+                but only if the above fails AND the Void Bag is open and enabled.
+            */
+            if (_voidBagEnabled && _rodItemIndex == -1 && Player.IsVoidVaultEnabled)
+            {
+                var manuallyCheckedIndex = Array.FindIndex(Player.bank4.item, i => i.netID == rodId);
+                if (manuallyCheckedIndex != -1)
+                {
+                    // Since we only manually check the Void Bag, the Rod Item is guaranteed to be in the Void Bag
+                    _isInVoidBag = true;
+                    _rodItemIndex = manuallyCheckedIndex;
+                }
+            }
+
+            if (_rodItemIndex != -1)
+            {
+                // Swap the items at the given index between the Player's main inventory and their Void Bag
+                if (_isInVoidBag)
+                {
+                    _swapItem = Player.inventory[_rodItemIndex];
+                    _rodItem = Player.bank4.item[_rodItemIndex];
+                    Player.inventory[_rodItemIndex] = Player.bank4.item[_rodItemIndex];
+                }
+
+                Player.selectedItem = _rodItemIndex;
+                Player.controlUseItem = true;
             }
         }
     }
